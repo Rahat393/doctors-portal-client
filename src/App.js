be33775f2b2ef06b860e11ed45d8e6032016e7ea
unknown <rahataminulislam37@gmail.com>
2023-01-13 22:38:53 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <h2>hello world</h2>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+    <div className='max-w-screen-xl  mx-auto'  >
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
