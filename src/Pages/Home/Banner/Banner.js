e5e7eabf0f88efdb8f5b66e3200c6d02f4d986e2
unknown <rahataminulislam37@gmail.com>
2023-01-14 00:33:46 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import img from '../../../assets/images/chair.png'
+import bimg from '../../../assets/images/bg.png'
+
 import './Banner.css'
+
 const Banner = () => {
     return (
-        <div className="hero  ">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={img} alt='' className="max-w-sm rounded-lg shadow-2xl" />
+        <div className="hero my-12 " style={{ backgroundImage: `url(${bimg})` }}>
+            <div className="hero-content p-12 flex-col lg:flex-row-reverse"  >
+                <img src={img} alt='' className="max-w-md rounded-lg shadow-2xl" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p className="py-6 lg:w-2/3">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import InfoCard from './InfoCard/InfoCard';
+import InfoCards from './InfoCard/InfoCards';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <InfoCards></InfoCards>
         </div>
     );
 };
