3fb2b4694b36a9479964079e3053180f278c6d15
unknown <rahataminulislam37@gmail.com>
2023-01-14 12:12:25 +0600

    services added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,16 +3,17 @@ import img from '../../../assets/images/chair.png'
 import bimg from '../../../assets/images/bg.png'
 
 import './Banner.css'
+import BtnPrimary from '../../../Components/BtnPrimary';
 
 const Banner = () => {
     return (
-        <div className="hero my-12 " style={{ backgroundImage: `url(${bimg})` }}>
+        <div className="hero my-12 acc" style={{ backgroundImage: `url(${bimg})` }}>
             <div className="hero-content p-12 flex-col lg:flex-row-reverse"  >
                 <img src={img} alt='' className="max-w-md rounded-lg shadow-2xl" />
                 <div>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6 lg:w-2/3">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <BtnPrimary className="btn btn-primary">Get Started</BtnPrimary>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import InfoCard from './InfoCard/InfoCard';
 import InfoCards from './InfoCard/InfoCards';
+import Services from './Service/Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <InfoCards></InfoCards>
+            <Services></Services>
         </div>
     );
 };
