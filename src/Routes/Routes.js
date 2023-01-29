4f5620d86512240727df9c166ffbabdd21ef46e1
Rahat393 <rahataminulislam37@gmail.com>
2023-01-29 04:35:03 -0800

    my appointment added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,38 +1,55 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../Layouts/DashboardLayout";
 import Main from "../Layouts/Main";
 import Appointment from "../Pages/Appointment/Appointment";
 import Dashboard from "../Pages/Dashboard/Dashboard/Dashboard";
+import MyAppointment from "../Pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../Pages/Home/Home";
 import LogIn from "../Pages/LogIn/LogIn";
 import SignUp from "../Pages/SignUp/SignUp";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: 'login',
-                element: <LogIn></LogIn>
-            },
-            {
-                path: 'signup',
-                element: <SignUp></SignUp>
-            },
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <LogIn></LogIn>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
+      },
 
-            {
-                path: '/appointment',
-                element: <PrivateRoute><Appointment></Appointment></PrivateRoute>
-            }
-        ]
-    },
-    {
-        path: '/dashboard',
-        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>
-    }
-])
+      {
+        path: "/appointment",
+        element: (
+          <PrivateRoute>
+            <Appointment></Appointment>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoute>
+        {" "}
+        <DashboardLayout></DashboardLayout>
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "/dashboard",
+        element:  <MyAppointment></MyAppointment> ,
+      },
+    ],
+  },
+]);
